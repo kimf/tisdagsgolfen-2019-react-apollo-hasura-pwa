f7f5e9f057f61957979ad442bdfcd1c52f798901
Kim Fransman <kim.fransman@gmail.com>
2018-12-01 19:52:45 +0100

    fix some node packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense, lazy } from "react";
-import { Router, Link } from "@reach/router";
+import { Router } from "@reach/router";
 import { ApolloProvider } from "react-apollo";
 
 import client from "./lib/apollo-client";
